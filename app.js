861900ded19ab00501d37ec76bbebb2ebcf4958e
kota661 <>
2020-02-25 18:41:03 +0900

    develop

--- app.js
+++ app.js
@@ -27,4 +27,5 @@ app.listen(port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
   console.log("webhook test2");
+  console.log("develop");
 });
